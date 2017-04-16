52afc474985401f9dc79af1bb3f526fd74a29166
Braccus <vboltunov@gmail.com>
2017-04-16 23:23:21 +0300

    Update tails.js

--- tails.js
+++ tails.js
@@ -3,15 +3,20 @@ console.log("")
 console.log("Правила игры: введите \"0\", если вы выбираете \"решку\"; если ваш выбор - \"орел\", тогда введите \"1\". После этого игра подбрасывает монетку и, если вы угадали результат броска, то вы выиграли, а если не угадали, тогда проиграли.");
 console.log("");
 
-//Просим игрока сделать выбор и выводим его в консоль
+headsOrTails();
+
+let wins = 0; // Количество выигрышей
+
 function headsOrTails() {
-    var readline = require('readline');
-    var rl = readline.createInterface({
-        input: process.stdin, // ввод из стандартного потока
-        output: process.stdout // вывод в стандартный поток
+    let readline = require('readline');
+    let rl = readline.createInterface({
+        input: process.stdin, // Ввод из стандартного потока
+        output: process.stdout // Вывод в стандартный поток
     });
 
+    let fs = require("fs");
 
+    //Просим игрока сделать выбор и выводим его в консоль
     rl.question("Орел или решка? ", function (answer) {
 
         if (answer == 0) {
@@ -27,7 +32,7 @@ function headsOrTails() {
 
         // Бросаем монетку и выводим результат броска
         function flipCoin() {
-            var coin = Math.floor(Math.random() * 2);
+            let coin = Math.floor(Math.random() * 2);
             if (coin == 0) {
                 console.log("Результат броска монетки: решка");
             } else {
@@ -38,11 +43,13 @@ function headsOrTails() {
         // Сравниваем выбор игрока с результатом броска монетки и выводим результат игры
         if (answer == flipCoin()) {
             console.log("Вы выиграли!");
+            wins++; // Складываем выигрыши
+            fs.writeFile("results.txt", wins); // Пишем текущее количество выигрышей в файл
         } else {
             console.log("Вы проиграли!");
         }
 
-        // Запрос на продолжение игры
+        // Спрашиваем игрока, желает ли он сыграть еще раз
         rl.question("Сыграем еще?(y/n) ", function (answer) {
             if (answer == "y") {
                 rl.close();
@@ -50,12 +57,16 @@ function headsOrTails() {
             } else if (answer == "n") {
                 console.log("Спасибо за игру!");
                 rl.close();
+                let data = fs.readFileSync("results.txt", "utf8", function(err, contents){
+                    console.log(contents);
+                });
+                console.log("Количество выигрышей: ", data);
+                fs.unlink("results.txt");
             } else {
                 console.log("Выбор неверный. Игра закрывается!")
                 rl.close();
+                fs.unlink("results.txt");
             }
         });
     });
 }
-
-headsOrTails();
